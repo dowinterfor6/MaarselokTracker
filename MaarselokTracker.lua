b36868854a0ec1ef584ffa067630c82079ed6d83
dowinterfor6 <dowinterfor6@gmail.com>
2019-07-09 13:50:01 -0700

    Add basic countdown functionality, currently commented out

--- MaarselokTracker.lua
+++ MaarselokTracker.lua
@@ -1,8 +1,11 @@
 -- Create namespace with top level table
 MaarselokTracker = {}
 
--- Constant for use later
+-- Constants
 MaarselokTracker.name = "MaarselokTracker"
+MaarselokTracker.procCooldown = 7
+MaarselokTracker.timer = 0
+MaarselokTracker.UPDATE_INTERVAL = 100 
 
 -- Restore saved position from savedVariables
 function MaarselokTracker:RestorePosition()
@@ -24,6 +27,9 @@ function MaarselokTracker:Initialize()
     EVENT_PLAYER_COMBAT_STATE, 
     self.OnPlayerCombatState
   )
+
+  EVENT_MANAGER:RegisterForEvent(MaarselokTracker.name, EVENT_COMBAT_EVENT, MaarselokTracker.OnCombatEvent)
+  -- Filter for the right id eventually
 
   -- Initialie saved variables
   self.savedVariables = ZO_SavedVars:NewAccountWide(
@@ -67,5 +73,48 @@ function MaarselokTracker.OnIndicatorMoveStop()
   MaarselokTracker.savedVariables.top = MaarselokTrackerIndicator:GetTop()
 end
 
+-- Callback for checking MaarselokAbilityId
+function MaarselokTracker.OnCombatEvent(eventCode, result, isError, abilityName, abilityGraphic, abilityActionSlotType, sourceName, sourceType, targetName, targetType, hitValue, powerType, damageType, log, sourceUnitId, targetUnitId, abilityId)
+-- Use this template when done debugging
+-- function MaarselokTracker.OnCombatEvent(_, _, _) etc
+  -- eventCode, result, isError, abilityName, abilityGraphic, abilityActionSlotType, sourceName, sourceType, targetName, targetType, hitValue, powerType, damageType, log, sourceUnitId, targetUnitId, abilityId
+  
+  d("result=" .. tostring(result) .. "; isError=" .. tostring(isError) .. "; abilityName=" .. tostring(abilityName)
+          .. "; abilityGraphic=" .. tostring(abilityGraphic) .. "; abilityActionSlotType=" .. tostring(abilityActionSlotType)
+          .. "; sourceName=" .. tostring(sourceName) .. "; sourceType=" .. tostring(sourceType) .. "; targetName=" .. tostring(targetName)
+          .. "; targetType=" .. tostring(targetType) .. "; hitValue=" .. tostring(hitValue) .. "; powerType=" .. tostring(powerType)
+          .. "; damageType=" .. tostring(damageType) .. "; log=" .. tostring(log) .. "; sourceUnitId=" .. tostring(sourceUnitId)
+          .. "; targetUnitId=" .. tostring(targetUnitId) .. "; abilityId=" .. tostring(abilityId))
+  d("------------")
+
+  d("result=" .. tostring(result) .. "; abilityName=" .. tostring(abilityName)
+              .. "; hitValue=" .. tostring(hitValue) .. "; powerType=" .. tostring(powerType)
+              .. "; damageType=" .. tostring(damageType) .. "; abilityId=" .. tostring(abilityId))
+  d("------------")
+
+  MaarselokTrackerIndicatorLabel:SetText(abilityName)
+
+  -- -- If it is maarselok
+  -- MaarselokTracker.timer = MaarselokTracker.procCooldown
+  -- MaarselokTrackerIndicatorTimer:SetText(MaarselokTracker.timer)
+  -- EVENT_MANAGER:RegisterForUpdate(
+  --   MaarselokTracker.name,
+  --   MaarselokTracker.UPDATE_INTERVAL,
+  --   MaarselokTracker.countDown
+  -- )
+end
+
+-- Count down the timer
+function MaarselokTracker.countDown()
+  if MaarselokTracker.timer > 0 then
+    MaarselokTracker.timer -= MaarselokTracker.UPDATE_INTERVAL
+  else
+    MaarselokTracker:UnregisterForUpdate(
+      MaarselokTracker.name
+    )
+  end
+  MaarselokTrackerIndicatorTimer:SetText(string.format("%.1f", MaarselokTracker.timer))
+end
+
 -- Register event listener with namespace, event, and callback
-EVENT_MANAGER:RegisterForEvent(FooAddon.name, EVENT_ADD_ON_LOADED, MaarselokTracker.OnAddOnLoaded)
+EVENT_MANAGER:RegisterForEvent(MaarselokTracker.name, EVENT_ADD_ON_LOADED, MaarselokTracker.OnAddOnLoaded)
